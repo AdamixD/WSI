2a261be0d3020ca23f60d3a5e74e46e54fd7ea81
Adam Dąbkowski <adamdobrus@gmail.com>
2022-11-05 22:49:41 +0100

    Add: - class GeneticAlgorithmSolver - class GeneticAlgorithmAnalyzer - start writing the report

--- Lab2/utils.py
+++ Lab2/utils.py
@@ -1,54 +1,30 @@
-import random
-
 import numpy as np
 
 
-def create_population(population_size=20, individual_size=10):  # TODO zmienić domyślne wartości
-    population = []
+class GeneticAlgorithmAnalyzer:
+    def __init__(self, solver, problem, pop0, n_samples):
+        self._solver = solver
+        self._problem = problem
+        self._pop0 = pop0
+        self._n_samples = n_samples
+        self._X_best = []
+        self._O_best = []
 
-    for i in range(population_size):
-        individual = []
-        for j in range(individual_size):
-            individual.append(random.randint(0, 1))
-        population.append(np.array(individual))
+    def analyze(self):
+        for i in range(self._n_samples):
+            x_best, o_best = self._solver.solve(self._problem, self._pop0)
+            self._X_best.append(x_best)
+            self._O_best.append(o_best)
 
-    return np.array(population)
+            print(f"Sample {i + 1}: Best score = {o_best}")
 
+        o_best_total = np.max(self._O_best)
+        o_min_total = np.min(self._O_best)
+        o_best_total_index = np.argmax(self._O_best)
+        average_score = np.average(self._O_best)
+        median_score = np.median(self._O_best)
 
-def q(x):  # TODO a co w sytuacji, gdy prędkość będzie większa niż abs(v0) < 2 ?
-    h0 = 200
-    v0 = 0
-    fuel = x.sum()
-    m0 = 200 + fuel
-
-    for i in x:
-        m0 -= i * 1
-        a = (45 / m0) * i - 0.09
-        v0 = v0 + a
-        h0 = h0 + v0 + a / 2
-
-        if (0 <= h0 < 2) and abs(v0) < 2:
-            return 2000 - fuel
-
-        elif h0 < 0:
-            return -1000 - fuel
-
-    return - fuel
-
-
-def score(q, P):
-    return [q(x) for x in P]
-
-
-def find_best(P, O):
-    index = np.argmax(O)
-    return P[index], O[index]
-
-
-def selection(P, O, u):
-    O = [o + (1000 + P.shape[1]) for o in O]
-    O = [o / sum(O) for o in O]
-    indices = np.arange(0, P.shape[0])
-    selected_indices = np.random.choice(indices, u, replace=True, p=O)
-
-    return P[selected_indices]
+        print(' ')
+        print(f'Best sample = {o_best_total_index + 1}')
+        print(f'Best score = {o_best_total}, Minimum score = {o_min_total}')
+        print(f'Average score = {average_score}, Median score = {median_score}')
